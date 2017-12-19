93dbf1df0ea8f1612ec03b49909bb73413111b5b
Ben <Benjimancorso@gmail.com>
2017-12-18 20:00:53 -0500

    added hash code

--- AuthenticationServer/Utility.cpp
+++ AuthenticationServer/Utility.cpp
@@ -35,8 +35,19 @@ std::string createSalt()
 char * createHash(char* pass) {
 	//use sha_256 to generate a hash of the passed in string
 	unsigned char digest[SHA256_DIGEST_LENGTH];
+
+	//SHA256((unsigned char*)&pass, strlen(pass), (unsigned char*)&digest);
+
+	SHA256_Init(&ctx);
 	SHA256_Update(&ctx, pass, strlen(pass));
 	SHA256_Final(digest, &ctx);
+
+	char mdString[SHA256_DIGEST_LENGTH * 2 + 1];
+	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+		sprintf(&mdString[i * 2], "%02x", (unsigned int)digest[i]);
+
+	//SHA256_Update(&ctx, pass, strlen(pass));
+	//SHA256_Final(digest, &ctx);
 	//return the hash
-	return pass;
+	return mdString;
 }
